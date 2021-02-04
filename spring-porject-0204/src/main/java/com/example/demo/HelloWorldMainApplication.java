8c1b44e38ef6c06e22baae23314a6d75dd957c30
sro800313 <sro800313@gmail.com>
2021-02-04 16:03:03 +0800

    the first spring boot application

---
+++ spring-porject-0204/src/main/java/com/example/demo/HelloWorldMainApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+
+	public static void main(String[] args) {
+		
+		//spring應用啟動
+		SpringApplication.run(HelloWorldMainApplication.class, args);
+	}
+	
+}
---
+++ spring-porject-0204/src/main/java/com/example/demo/SpringPorject0204Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPorject0204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPorject0204Application.class, args);
+	}
+
+}
